11628472e6b82d573ffab05b2d69e9c441c416e7
Marc <mesolidum14@gmail.com>
2024-10-31 02:17:47 +0800

    module 3

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'includes')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
