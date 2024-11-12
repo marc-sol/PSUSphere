5d35ac915f184594c966c08e8012ef6b525a12ab
Marc <mesolidum14@gmail.com>
2024-11-13 02:36:40 +0800

    minor updates

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -123,16 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
-    BASE_DIR / 'PSUSphere/projectsite/static', 
+    BASE_DIR / 'static', 
 )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGOUT_REDIRECT_URL = 'login' 
+LOGOUT_REDIRECT_URL = '/login' 
 LOGIN_REDIRECT_URL = 'home' 
 LOGIN_URL = 'login'
