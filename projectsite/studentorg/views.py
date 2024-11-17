8fbfa113e0d621ec3c520594a7bac48f3772c553
Marc <mesolidum14@gmail.com>
2024-11-18 00:18:18 +0800

    dynamic pie chart

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -16,6 +16,11 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.db.models import Q
 from django.urls import reverse_lazy
+from django.db import connection 
+from django.http import JsonResponse 
+from django.db.models.functions import ExtractMonth
+from django.db.models import Count 
+from datetime import datetime
 from django.utils.decorators import method_decorator 
 from django.contrib.auth.decorators import login_required
 
@@ -34,6 +39,21 @@ class ChartView(ListView):
     
     def get_queryset(self, *args, **kwargs): 
         pass
+
+def PieCountbySeverity(request): 
+    query = ''' 
+    SELECT severity_level, COUNT(*) as count 
+    FROM fire_incident GROUP BY severity_level 
+    ''' 
+    data = {} 
+    with connection.cursor() as cursor: 
+        cursor.execute(query) 
+        rows = cursor.fetchall()
+    if rows: # Construct the dictionary with severity level as keys and count as values 
+        data = {severity: count for severity, count in rows} 
+    else: 
+        data = {}
+    return JsonResponse(data)
 
 class OrganizationList(ListView): 
     model = Organization 
