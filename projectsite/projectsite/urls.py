ba488763b19c82d795b8693215f0df674915f92e
Marc <mesolidum14@gmail.com>
2024-11-26 21:27:27 +0800

    minor changes

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -19,16 +19,10 @@ from django.urls import path, re_path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView,  OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView, CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView, ProgramList, ProgramCreateView, ProgramUpdateView
 from studentorg import views
 from django.contrib.auth import views as auth_views
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path('', views.HomePageView.as_view(), name='home'),
-    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('chart/', PieCountbySeverity, name='chart'),
-    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
-    path('multiBarChart/', multipleBarbySeverity, name='chart'),
-    path('stations', views.map_station, name='map-station'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
